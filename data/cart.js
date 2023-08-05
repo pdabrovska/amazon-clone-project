a3acd4cdeb22b4a390db22b76d36b43409f42a30
Paulina Dąbrowska <paulina.dabrovska@gmail.com>
2023-08-05 19:55:38 +0200

    added an update quantity feature

--- data/cart.js
+++ data/cart.js
@@ -57,3 +57,15 @@ export function removeFromCart(productId){
   saveToStorage();
   updateCartQuantity('.js-products-quantity');
 };
+
+export function updateQuantity(productId, newQuantity){
+  let matchingItem;
+  cart.forEach((cartItem) =>{
+    if(cartItem.productId === productId){
+      matchingItem = cartItem
+    }
+  });
+
+  matchingItem.quantity = newQuantity;
+  saveToStorage();
+};
